ff953938a98d10ee4f7fc93d7a17ff5216660ac8
Kwill <kaiqueprado262@gmail.com>
2025-03-27 14:23:44 -0300

    faltou isso

--- src/app/(categorias)/series/componentes/lista.tsx
+++ src/app/(categorias)/series/componentes/lista.tsx
@@ -82,7 +82,7 @@ export default function Lista() {
           })}
       </ul>
 
-      {filteredData.length > 0 && (
+      {searchQuery && filteredData.length > 0 && (
         <button
           className="mt-4 px-4 py-2 border border-blue-500 text-white rounded-lg hover:bg-blue-700"
           onClick={handleTmdbSearch}
